351e2e360faad815b4e3f47ff2bdaf0657c0ffac
Juliana Bronqueti da Silva <msft39@joyce.LABS-IC>
2022-09-03 12:16:16 -0300

    Collecting the user inicial board inputs

---
+++ jewel_collector/jewel_collector/JewelCollector.cs
@@ -0,0 +1,51 @@
+﻿public class JewelCollector {
+
+    public static void Main() {
+
+        bool running = true;
+
+        Console.WriteLine("Enter the board dimension: ");
+        int dimension = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Insert the number of jewels: ");
+        int n_jewels = Convert.ToInt32(Console.ReadLine());
+
+        for(int i=0; i<n_jewels; i++){
+            Console.WriteLine("Insert the type of jewel and position: ");
+            string jewel_position = Console.ReadLine();
+        }
+
+        Console.WriteLine("Insert the number of obstacles: ");
+        int n_obstacles = Convert.ToInt32(Console.ReadLine());
+
+        for(int i=0; i<n_obstacles; i++){
+            Console.WriteLine("Insert the type of jewel and position: ");
+            string obstacle_position = Console.ReadLine();
+        }
+
+        Console.WriteLine("Insert the robot inicial position: ");
+        string robot_position = Console.ReadLine();
+
+        Console.WriteLine("Let's start!!");
+
+        do {
+
+            Console.WriteLine("Enter the command: ");
+            string command = Console.ReadLine();
+
+            if (command.Equals("quit")) {
+                running = false;
+            } else if (command.Equals("w")) {
+                
+            } else if (command.Equals("a")) {
+                
+            } else if (command.Equals("s")) {
+            
+            } else if (command.Equals("d")) {
+            
+            } else if (command.Equals("g")) {
+                
+            }
+        } while (running);
+    }
+}
