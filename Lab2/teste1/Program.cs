66199f63b8317724123d3a0b2e81b4091ed10820
Juliana Bronqueti da Silva <msft39@joyce.LABS-IC>
2022-09-03 12:03:18 -0300

    Doing lab2 and creating the jewel collector (final project) folder and program

--- Lab2/teste1/Program.cs
+++ Lab2/teste1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿BorderSides leftRight = BorderSides.Left | BorderSides.Right;   // Enum combinados via operador OR binário
+Console.WriteLine(leftRight);
+
+if ((leftRight & BorderSides.Left) != 0)
+  Console.WriteLine("Includes Left");                           // Includes Left
+
+Console.WriteLine(leftRight.ToString());                        // 3 = "Left, Right"
+
+BorderSides s = BorderSides.Left;
+s |= BorderSides.Right;
+Console.WriteLine(s);
+Console.WriteLine(s == leftRight);                              // True
+
+s ^= BorderSides.Right;                                         // Remove Right
+Console.WriteLine(s);                                           // Left
+
+public enum BorderSides { None = 0, Left = 1, Right = 2, Top = 4, Bottom = 8 }
